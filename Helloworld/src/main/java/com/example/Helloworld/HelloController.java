5aa2740373edda96c25d5736d44e66e7547c1b1e
Ignaci Ignatius <ijignatius69@gmail.com>
2026-07-26 12:30:11 +0000

    Spring Boot Cloud Foundry changes

---
+++ Helloworld/src/main/java/com/example/Helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.Helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello SAP BTP Cloud Foundry";
+    }
+
+}
